fd120f06570ffccb79ad93039540a9b394b1303b
unknown <danbuitrago87@gmail.com>
2024-09-15 14:48:25 -0500

    DesafioUnoPeliculas

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Bienvenido a Screen Match");
+        System.out.println("Pelicula: Matrix");
+        int FechaDeLanzamiento=1999;
+        boolean IncluidoPlan= true;
+        double Calificacion=8.2;
+        double Media=(8.2+6.0+9.0)/3;
+        System.out.println("Media: "+Media);
+    }
+}
