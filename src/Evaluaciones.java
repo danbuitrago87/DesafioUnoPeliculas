7609db8c9a3ca0675859d35c6cc6aa6548065c6f
unknown <danbuitrago87@gmail.com>
2024-10-02 17:08:26 -0500

    Agregando Evaluaciones a través de while

---
+++ src/Evaluaciones.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Evaluaciones {
+    public static void main (String[] args){
+        Scanner scanner=new Scanner(System.in);
+        double nota=0;
+        double mediaEvaluaciones=0;
+        double totalEvaluaciones=0;
+        while (nota!=-1){
+            System.out.println("Digite la nota de la pelicula: ");
+            nota=scanner.nextDouble();
+            if(nota!=-1){
+                mediaEvaluaciones+=nota;
+                totalEvaluaciones++;
+            }
+        }
+        System.out.println("La media de evaluaciones para la pelicula es: "+mediaEvaluaciones/totalEvaluaciones);
+    }
+}
